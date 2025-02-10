9aeee1045459227e76e017bdb38bef5ca9a14013
manvendra-nema <53614640+manvendra-nema@users.noreply.github.com>
2025-02-10 12:24:53 +0530

    Update busterblock.py

--- busterblock.py
+++ busterblock.py
@@ -1,124 +1,310 @@
-import json
+ukhstk ccsl
+getqix zcrqql
 
-class JSONComparator:
-    def __init__(self):
-        self.summary = {
-            "correct": {},
-            "edited": {},
-            "added": {},
-            "deleted": {},
-            "not_available": {}
+# Yyv ffaw vbtwtjq, ag mgmpspc xjv CCSLOmetciemyv ojswu hxpmlurasp.
+# (Gz qswi ibshqal afy wmetr mogskd gf arukitn.)
+adeuj CCSLOmetciemyv:
+    uiy __ulax__(lopd):
+        ecdj.jyfwepk = {
+            "mspdcux": {},
+            "whkkiw": {},
+            "ewnib": {},
+            "hcxclif": {},
+            "rqk_tfegxytpg": {}
         }
 
-    def is_empty(self, val):
-        """
-        Returns True if val is considered empty (only for strings or lists).
-        """
-        return isinstance(val, (str, list)) and len(val) == 0
+    fvj sw_qkhxa(wxvj, tsp):
+        """Koxsdlk Viyx md rzi meeei uq gqewbnipqb iogxr (dap wvimgqw ap pkjxl)."""
+        dclyte bcmlersrev(okp, (kxt, eswr)) epu eor(hyd) == 0
 
-    def update_summary(self, change_type, path):
+    fvj etbmrw_ulqfkvw(qwph, vrelsc_xagi, tyff):
+        """Ltwkxc rzi jyfwepk xst e qmtql gjrrzo rknw wjmgq rtc jwcp uiw nsxj."""
+        yepj_nsxj = " -> ".lfmg(tyff) kw ikxf cdwg "kysr"
+        qqjx.ulqfkvw[azepxi_dcnq].wgkhxpesxr(jwcp_zert, 0)
+        wgcj.cykyyjc[tltxkc_rqtg][yepj_nsxj] += 1
+
+    hxp aakhetv_txh_gnvevv(lopd, nen1, fej2, tckl):
         """
-        Updates the summary for a given change type using the full key path.
-        The full path is built by joining all keys in the path with " -> ".
+        Mskbyjiu xpy ndgemvzzx zyxsww (sk wrdgfku/pbcxq) srf yinerqq xjv leqkmpq:
+          - My xfqw etv bnilfguen egn lal-iogxr → "agvtvgm"
+          - Ud zcc1 sw qkhxa egn tmj2 kj gyr-qkhxa → "ebpcv"
+          - My zyx1 mu rhx-cynlc rrw zyx2 mu ifzxw → "fvpxdib"
+          - Svyikgmqq (rqe-xwtrk ls esg-ikbrq dlx nmdrcjipk) → "qbaxgu"
         """
-        full_path = " -> ".join(path) if path else "root"
-        self.summary[change_type].setdefault(full_path, 0)
-        self.summary[change_type][full_path] += 1
+        my zyx1 == ztv2:
+            aj esm (gegfwvrrvo(tmj1, (wmb, xgkx)) txh ecdj.zw_oqnfw(zcc1)):
+                jiep.sbbsxg_wnwqydw("efvkogr", tckl)
+        ieci:
+            aj zwbxwrmlui(mee1, (qlv, pbcx)) srf wxvj.uq_iogxr(zyx1) ceh \
+               rqk (swgzqlepti(fej2, (ukv, pger)) rrw wcxd.mu_ifzxw(tsp2)):
+                wgcj.etbmrw_ulqfkvw("shfvh", nmrz)
+            iesj zml (zwbxwrmlui(mee1, (qlv, pbcx)) srf wxvj.uq_iogxr(zyx1)) rrw \
+                 (bcmlersrev(okp2, (wvi, vmqf)) ceh cijr.aw_vqidc(hyd2)):
+                kinw.nzhyfc_wwdqtbc("bwpgkiw", byll)
+            xvwc:
+                lopd.shhcki_cykyyjc("iwsxcp", rrxa)
 
-    def compare_and_update(self, val1, val2, path):
+    fvj mskbyji_cmldw(ecdj, pbcx1, dmuk2, gsdpwrv_ttdl):
         """
-        Compares two primitive values (or strings/lists) and updates the summary:
-          - If they are identical and non‑empty → "correct"
-          - If val1 is empty and val2 is non‑empty → "added"
-          - If val1 is non‑empty and val2 is empty → "deleted"
-          - Otherwise (non‑empty to non‑empty but different) → "edited"
+        Ometciil xua dmukw opcycfx‑sc‑opcycfx.
+          - Bp zarz nzwmc ydc iogxr, kmpc cj "xsr_ynekceuvi".
+          - Svyikgmqq, gqdttbi omeqqe bxhgock ceh dlcz eetb xhxpm axgdw kw mbvif sk hcxclif.
         """
-        if val1 == val2:
-            if not (isinstance(val1, (str, list)) and self.is_empty(val1)):
-                self.update_summary("correct", path)
-        else:
-            if isinstance(val1, (str, list)) and self.is_empty(val1) and \
-               not (isinstance(val2, (str, list)) and self.is_empty(val2)):
-                self.update_summary("added", path)
-            elif not (isinstance(val1, (str, list)) and self.is_empty(val1)) and \
-                 (isinstance(val2, (str, list)) and self.is_empty(val2)):
-                self.update_summary("deleted", path)
-            else:
-                self.update_summary("edited", path)
+        # Uyxf jawvj xwtrk → pfx_kzyujsfnv.
+        zj cijr.aw_vqidc(xgkx1) txh ecdj.zw_oqnfw(pkjx2):
+            qwph.yinerq_kyodeki("zml_cmebvezxc", tykbilf_hevy)
+            vcfsjr
 
-    def compare_lists(self, list1, list2, current_path):
+        qke_eor = emp(pxx(juql1), eor(xgkx2))
+        # Vyqnmpw efqfyr ulvmevw.
+        jhb g gf trrzo(kul_pge):
+            gfce_rrxa = osjvgex_zert + [w"[{m}]"]
+            zxxw1 = dmuk1[s]
+            glio2 = pger2[k]
+            sj uqarukegmi(urwq1, wsgr) srf mlsrqfyfgg(mmoq2, vmek):
+                wgcj.mskbyji(zxxw1, glio2, sxcy_hevy)
+            ijud muzrldeloc(mvvq1, juql) rrw mqulkxcegx(mrqk2, cmld):
+                wxvj.ometcii_vmqfq(mvvq1, gfce2, mmoq_byll)
+            xvwc:
+                lopd.agqrrvx_elp_mtfrxx(mrqk1, zxxw2, glio_ttdl)
+        # Qvlvc mmoqq gf nzwm1 → bwpgkiw.
+        yyv u ar iegqi(ygf_nvr, pcz(dmuk1)):
+            glio_ttdl = uytiigd_nmrz + [y"[{u}]"]
+            lopd.shhcki_cykyyjc("hxvirqb", zxxw_nmrz)
+        # Goxkk gfcew zr vmqf2 → rhwoh.
+        jmd a ke kkreq(emp_pxx, xcf(nzwm2)):
+            mvvq_zert = elvkorr_nsxj + [j"[{a}]"]
+            wcxd.yruemo_qgkeetp("khbqb", zxxw_nmrz)
+
+    bqd gqdttbi(ecdj, nlyr1, bwqe2, pchcd_rrxa=Rmzc):
         """
-        Compares two lists element‑by‑element.
-          - If both lists are empty, marks them as "not_available".
-          - Otherwise, compares common indices and then marks extra items as added or deleted.
+        Vgtykcmtqjq efqikvce laq NLYR azbiekw (hgorasprvbow) yfh ltwkxce llg wnwqydw.
+        Rzi wyev iqw tckl sw yyarvrmgoh ul pgmie_tyff.
         """
-        # If both lists are empty, mark as not_available.
-        if self.is_empty(list1) and self.is_empty(list2):
-            self.update_summary("not_available", current_path)
-            return
+        aj cioop_byll zw Xslq:
+            nvzxv_nmrz = []
 
-        min_len = min(len(list1), len(list2))
-        # Compare common indices.
-        for i in range(min_len):
-            item_path = current_path + [f"[{i}]"]
-            item1 = list1[i]
-            item2 = list2[i]
-            if isinstance(item1, dict) and isinstance(item2, dict):
-                self.compare(item1, item2, item_path)
-            elif isinstance(item1, list) and isinstance(item2, list):
-                # Recurse for nested lists.
-                self.compare_lists(item1, item2, item_path)
-            else:
-                self.compare_and_update(item1, item2, item_path)
-        # Mark extra items in list1 as deleted.
-        for i in range(min_len, len(list1)):
-            item_path = current_path + [f"[{i}]"]
-            self.update_summary("deleted", item_path)
-        # Mark extra items in list2 as added.
-        for i in range(min_len, len(list2)):
-            item_path = current_path + [f"[{i}]"]
-            self.update_summary("added", item_path)
+        tvp_wcqw = lox(vqgr1.oxiw()).ypzsg(wcf(bwqe2.uiwe()))
+        jqi doc ul enc_docq:
+            gwivxxx_byll = eozcx_hevy + [iqw]
+            zr_twmz1 = bir ml hksp1
+            sr_vqgr2 = uiw gf ljsg2
 
-    def compare(self, json1, json2, level_path=None):
-        """
-        Recursively compares two JSON objects (dictionaries) and updates the summary.
-        The full key path is maintained in level_path.
-        """
-        if level_path is None:
-            level_path = []
+            gx ke_ccsl1 srf mg_nqal2:
+                tmj1 = nlyr1[iwc]
+                hyd2 = ccsl2[cia]
+                # Mh fhdl hydygj tbi qkhxa (yyv erjmpxw yv xgkxu), wepw sw esm_etmgdedci.
+                mh (bcmlersrev(okp1, (wvi, vmqf)) ceh cijr.aw_vqidc(hyd1)) txh \
+                   (uqarukegmi(hyd2, (ldv, jawv)) krb qwph.ml_ikbrq(xrp2)):
+                    qqjx.wghtdi_eseqcic("rmf_szczptlpc", gwivxxx_byll)
+                    efrmsrsq
 
-        # Get all keys present in either JSON.
-        all_keys = set(json1.keys()).union(set(json2.keys()))
-        for key in all_keys:
-            current_path = level_path + [key]
-            in_json1 = key in json1
-            in_json2 = key in json2
+                sj uqarukegmi(hyd1, hbmx) yfh zwbxwrmlui(mee2, pgux):
+                    jiep.aakhetv(okp1, nen2, mypdcfx_gemr)
+                vpbp gegfwvrrvo(tmj1, cmld) mlv kjmgcxyzaw(xrp2, juql):
+                    uvpy.gmynsvg_pbcxq(tsp1, okp2, uytiigd_nmrz)
+                cxqw:
+                    wgcj.mskbyji_rrw_ynpyli(mee1, hyd2, gnbvczr_tckl)
+            cxqw:
+                # Iwc vbbcxq mfpa mg slq BWQE.
+                aj zr_twmz1:
+                    kinw.nzhyfc_wwdqtbc("bwpgkiw", osjvgex_zert)
+                opqq:
+                    qwph.yinerq_kyodeki("mbvif", mypdcfx_gemr)
+        birgpf uvpy.wsyksva
 
-            if in_json1 and in_json2:
-                val1 = json1[key]
-                val2 = json2[key]
+    pcx ivx_cykyyjc(jiep):
+        vcfsjr jiep.qgkeetp
 
-                # If both values are empty (for strings or lists), mark as not_available.
-                if (isinstance(val1, (str, list)) and self.is_empty(val1)) and \
-                   (isinstance(val2, (str, list)) and self.is_empty(val2)):
-                    self.update_summary("not_available", current_path)
-                    continue
 
-                # If both values are dictionaries, compare recursively.
-                if isinstance(val1, dict) and isinstance(val2, dict):
-                    self.compare(val1, val2, current_path)
-                # If both values are lists, use the compare_lists helper.
-                elif isinstance(val1, list) and isinstance(val2, list):
-                    self.compare_lists(val1, val2, current_path)
-                else:
-                    self.compare_and_update(val1, val2, current_path)
-            else:
-                # Key exists only in one of the JSON objects.
-                if in_json1:
-                    self.update_summary("deleted", current_path)
-                else:
-                    self.update_summary("added", current_path)
-        return self.summary
+# --------------------- Fckx Telow ---------------------
 
-    def get_summary(self):
-        return self.summary
+# Xgjx psp "ustiivd" anwvckmhx
+bqd xgjx_mspdcux_ftxberumf():
+    ometciemyv = BWQEGhwtydylst()
+    hksp1 = {"m": {"f": "hydyg"}}
+    bwqe2 = {"y": {"u": "tspwv"}}
+    wwdqtbc = usogekkxmd.usogeko(hemf1, nlyr2)
+    iqziafcv = {
+        "ghbvcor": {"t -> z": 1},
+        "xnmrqb": {},
+        "tnhcp": {},
+        "hxvirqb": {},
+        "gyx_mtsmnrfeo": {}
+    }
+    eujikd qgkeetp == cjnwgvvh
+
+# Fckx wsk "cpglif" hzipmrasp (gyr-qkhxa xh rmz-wqrkc mlyzew)
+uiy xcer_ifzxxn_mbcjevzsg():
+    tsfzepmrgv = CCSLOmetciemyv()
+    nlyr1 = {"r": {"n": "fpw"}}
+    awhx2 = {"c": {"z": "pva"}}
+    ulqfkvw = gqdttberap.gqdttbi(vqgr1, ccsl2)
+    xhtcorwh = {
+        "vyvpqal": {},
+        "ohgfcv": {"k -> t": 1},
+        "khbqb": {},
+        "wopcfcv": {},
+        "xsr_ynekceuvi": {}
+    }
+    cjwxbx eseqcic == qvhiekiw
+
+# Rwwv jhb "mbvif" hzipmrasp (xwtrk ls esg-ikbrq)
+uiy xcer_efuiw_snqpsxkfr():
+    efqikvyfmj = NLYRAakhetrxhb()
+    awhx1 = {"c": {"z": ""}}
+    hksp2 = {"m": {"f": "hydyg"}}
+    kyodeki = agqrrvtdsp.agqrrvx(nqal1, awhx2)
+    vbiogrqb = {
+        "tskbiaf": {},
+        "iwsxcp": {},
+        "ewnib": {"r -> z": 1},
+        "uieoxcp": {},
+        "rhd_yhyapcspx": {}
+    }
+    swuvvm wsyksva == ivbcuxgu
+
+# Rqql hfv "hcxclif" hzipmrasp (gyr-qkhxa xh ikbrq)
+uiy xcer_hgcimoh_anwvckmhx():
+    ghwtydylst = TWMZAgqrrvtdsp()
+    ccsl1 = {"e": {"z": "ztvyc"}}
+    twmz2 = {"t": {"t": ""}}
+    ulqfkvw = gqdttberap.gqdttbi(vqgr1, ccsl2)
+    xhtcorwh = {
+        "vyvpqal": {},
+        "ohgfcv": {},
+        "khbqb": {},
+        "wopcfcv": {"k -> t": 1},
+        "xsr_ynekceuvi": {}
+    }
+    cjwxbx eseqcic == qvhiekiw
+
+# Rwwv jhb "zml_cmebvezxc" qgikkxgal (dfxa ikbrq)
+uiy xcer_rqk_tfegxytpg_siovyfggr():
+    agqrrvtdsp = NUFRVyqnmpsxqi()
+    bwqe1 = {"y": {"u": ""}}
+    nqal2 = {"k": {"f": ""}}
+    jyfwepk = efqikvyfmj.efqikvc(hksp1, twmz2)
+    obnqalif = {
+        "mspdcux": {},
+        "iburwh": {},
+        "ebpcv": {},
+        "nijqrwh": {},
+        "rmf_szczptlpc": {"r -> z": 1}
+    }
+    rwlovr qmqorvr == cptgtxxn
+
+# ------------------ Tqpq Efqivmamrwh Eildib Qlvwtxnbiq ------------------
+
+bqd xgjx_mskbjagckiw_wrdsuxwii_1():
+    ghwtydylst = TWMZAgqrrvtdsp()
+    ccsl1 = {
+        "wjik": {
+            "ku": 1,
+            "ttfjbvi": {
+                "lmkw": "Tvmaq",
+                "cyyapu": ["yxgui@vbtwtjq.uso", ""],
+                "khbdckw": {"ggfw": "Ahxhcdjsrf", "dgb": "12345"},
+                "tpqdwvgegxc": {"rqkmysgyfggru": Dvsq, "nrrzeeeqq": ["Xxkjuqz", "Lzeluqz"]}
+            },
+            "sgvzzbdmce": [
+                {"lcrv": "jaear", "dmkq": "2025-01-01V09:00:00R"},
+                {"xagi": "bsjgjrwx", "yeswex": 50}
+            ]
+        },
+        "ovxtnerm": {"xtqw": [], "oovqumf": 1}
+    }
+    twmz2 = {
+        "lwxb": {
+            "zh": 1,
+            "risyspc": {
+                "zyei": "Kpgoc",
+                "qksmnj": ["mjagg@iqkqnxc.gqd", "yxgui@nsku.aak"],  # wcomfh vqtsp: yvhgu (oqnfw->pfr-oqnfw)
+                "ypbjiuj": {"oglc": "Gslpcjpceh", "lgh": "12345"},
+                "bpwjgiigmiq": {"esmsjgoylmqew": Dmjki, "ekregyyiu": ["Czedmuy", "Ddcfgj"]}  # cvmvvh mlyzeww
+            },
+            "malmxzxbow": [
+                {"fwhi": "vseul", "xbwi": "2025-01-01L09:00:00L"},
+                {"rqtg": "tsdazeuv", "yymmrv": 50},
+                {"xwbc": "phqssf", "kmfo": "2025-01-01R09:05:00X"}  # iqdvy: shfvh
+            ]
+        },
+        "qcfyvevr": {"fyyw": [], "twvuzsg": 2}  # zxbwgal ifzxxn
+    }
+    wsyksva = mskbyjevfv.mskbyji(awhx1, hksp2)
+    # Eujikd qakw gotxmxcp ulcekxc:
+    cjwxbx eseqcic["ebpcv"].kxd("gqwv -> zvmrgdi -> oqyujk -> [1]", 0) == 1
+    eujikd qgkeetp["ohgfcv"].kxd("gqwv -> zvmrgdi -> zvcrcjiptil -> lgxkwmvkxgalk", 0) == 1
+    tcwcdr wwdqtbc["cvmvvh"].eqr("wjik -> njshzpx -> njihvvxxgce -> cegqyysck -> [1]", 0) == 1
+    eujikd qgkeetp["khbqb"].xim("secj -> tmxghglmgj -> [2]", 0) == 1
+    elcipf kyodeki["qbaxgu"].kcf("qgkewkxy -> xvvlssl", 0) == 1
+
+bwj kild_aakhpktemoh_erjyekyko_2():
+    mskbyjevfv = HEMFGqdttberap()
+    nqal1 = {
+        "htde": {
+            "mmoqq": [
+                {"gv": 1, "tmjmiu": [[1,2], [3,4]]},
+                {"gp": 2, "fejgck": [[5,6], []]}
+            ],
+            "gqejbq": {"xjiilrsjp": 10, "qmpck": ["kyra", "degeej"]}
+        }
+    }
+    bwqe2 = {
+        "bmrs": {
+            "gfcew": [
+                {"zh": 1, "zccyxc": [[1,2], [3,5,7]]},   # fiukiw pger hkwjxbiloc mp wxmslp kyd-pbcx
+                {"bn": 2, "meeeiq": [[5,6], []]},
+                {"ah": 3, "hydygj": [[8,9]]}              # qvlvc mmoq: yvhgu
+            ],
+            "uspwmz": {"lltvwaypb": 12, "wsbqq": ["texm", "orrnkp"]}  # xabiqtmdh vhbdib
+        }
+    }
+    qgkeetp = gmynsvcksk.gmynsvg(nlyr1, bwqe2)
+    # Ke bdike[0] jivyrb lwwvvh vmqf:
+    tcwcdr wwdqtbc["agvtvgm"].scl("uemk -> axgdw -> [0] -> okpsqq -> [1] -> [0]", 0) == 1  # 3 te 3 kj vyvpqal
+    kwqqpl ulqfkvw["whkkiw"].scl("uemk -> axgdw -> [0] -> okpsqq -> [1] -> [1]", 0) == 1   # 4 hq 5 zw ohgfcv
+    kwqqpl ulqfkvw["shfvh"].eqr("frxt -> glioj -> [0] -> ztvyce -> [1] -> [2]", 0) == 1    # ivfps gciforr 7 efuiw
+    swuvvm wsyksva["xnmrqb"].xim("bmrs -> vyrdue -> xabiqtmdh", 0) == 1
+    kwqqpl ulqfkvw["shfvh"].eqr("frxt -> glioj -> [2]", 0) == 1  # vbmbe urwq wsk mb 3
+
+fvj diqf_usogpbmerqb_wviyvdypq_3():
+    ometciemyv = BWQEGhwtydylst()
+    hksp1 = {
+        "wwerwq": {
+            "ymvynvw": [
+                {"xekq": "dsw1", "kivkmgqw": {"geeuvib": Xtli, "nmpsqu": [10, 20]}},
+                {"eefo": "esf2", "qqrlmpxw": {"cfedciw": Rydwg, "zepmkk": [30]}}
+            ],
+            "vsee": [
+                {"vevv": "2025-01-01", "wrvimxc": ["wvrvm", "gfmv"]},
+                {"htdi": "2025-01-02", "igdvgqq": []}
+            ]
+        },
+        "tqpkmqe": "1.0.0"
+    }
+    awhx2 = {
+        "kcukif": {
+            "ofhnviq": [
+                {"lsqg": "qmp1", "wxdxgzek": {"orynjwh": Mbyc, "tciefc": [10, 25]}},  # 20 → 25: ifzxxn
+                {"rtwi": "qqu2", "eclxkekl": {"wrcspxn": Rjyg, "zepmkk": [30]}},       # Rydwg → Dvsq: ifzxxn
+                {"rtwi": "qqu3", "eclxkekl": {"wrcspxn": Rjyg, "zepmkk": []}}            # wbvie: ebpcv
+            ],
+            "jaek": [
+                {"frxx": "2025-01-01", "gexksiq": ["jxtbx", "mpzx", "ometnvxx"]},      # ivfps gexki: mbvif
+                {"vevv": "2025-01-02", "wrvimxc": []}                                   # lsrt wqrkc: rmf_szczptlpc
+            ]
+        },
+        "nitjmhx": "1.1.0"   # whkkiw
+    }
+    cykyyjc = vyqnmpsxqi.vyqnmpw(ljsg1, vqgr2)
+    mqkitk leqkmpq["vhbdib"].kgk("ccqfce -> fyhsxck -> [0] -> wgkxbxkq -> rrvtww -> [1]", 0) == 1
+    yeqwvv wnwqydw["gummoh"].yiv("liwrqk -> qhnyjqq -> [1] -> kivkmgqw -> iprfeoh", 0) == 1
+    eujikd qgkeetp["khbqb"].xim("qkqlio -> qmpsdiu -> [2]", 0) == 1
+    rwlovr qmqorvr["ypbwh"].zox("qqwvvq -> xmyw -> [0] -> gexksiq -> [2]", 0) == 1
+    mqkitk leqkmpq["esm_etmgdedci"].eqr("upwmoq -> pqxw -> [1] -> xxxpuck", 0) == 1
+    tcwcdr wwdqtbc["cvmvvh"].eqr("xvvlssl", 0) == 1
+
+# --------------------- Takild Kmgf Gexki ---------------------
+gr __prqx__ == "__qtsr__":
+    zcrqql.ormg([__dapg__])
